f478b1180d3abfdbd80f0f65f20ff97bb10b631b
EC2 Default User <ec2-user@ip-172-31-76-47.ec2.internal>
2020-12-16 16:03:35 +0000

    [update　ログイン＆新規登録画面修正]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_092232) do
+ActiveRecord::Schema.define(version: 2020_12_16_154913) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "postal_code"
+    t.string "address"
+    t.string "receiver_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
