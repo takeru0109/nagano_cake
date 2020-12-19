1749d675c28abc6afefafd6deb28a416fbfbe697
EC2 Default User <ec2-user@ip-172-31-76-47.ec2.internal>
2020-12-19 10:50:49 +0000

    [add オーダーアイテムモデル、コントローラ]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_103612) do
+ActiveRecord::Schema.define(version: 2020_12_19_104435) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "postal_code"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2020_12_19_103612) do
     t.string "image_id"
     t.boolean "status", default: true
     t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "making_status", default: 0
+    t.integer "item_count"
+    t.integer "ordered_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
