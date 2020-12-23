768e459c2d86e50d76bc580ba824a291479b017f
EC2 Default User <ec2-user@ip-172-31-76-47.ec2.internal>
2020-12-23 15:09:41 +0000

    add userコントローラ

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomesController < ApplicationController
+  def top
+    @genres = Genre.where(status: true)
+    @item1 = Item.first
+    @item2 = Item.second
+    @item3 = Item.third
+    @item4 = Item.fourth
+  end
+
+  def about
+  end
+end
